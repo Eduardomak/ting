e116633b1d9cc0fd9402c9f42ea5ec29421ffa2f
Eduardomak <eduardomak92@gmail.com>
2023-02-13 13:44:15 -0300

    feat: add word_searches

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    search = []
+    line = 1
+    for index in range(len(instance)):
+        file_search = instance.search(index)
+
+        expected_result = {
+            "palavra": word,
+            "arquivo": file_search["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for _ in file_search["linhas_do_arquivo"]:
+            if word in _.lower():
+                expected_result["ocorrencias"].append({"linha": line})
+                search = [expected_result]
+            line += 1
+
+    return search
 
 
 def search_by_word(word, instance):
