b18d099edc22db86e78c0cdc96bc597ab492179a
Eduardomak <eduardomak92@gmail.com>
2023-02-13 13:43:17 -0300

    feat: add file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            data = file.readlines()
+            required_export = [line.replace("\n", "") for line in data]
+            return required_export
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for _ in instance._Queue__queue:
+        if _["nome_do_arquivo"] == path_file:
+            return
+    queue_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    instance.enqueue(queue_dict)
+    print(instance.__dict__, file=sys.stdout)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    try:
+        path_file = instance.dequeue()["nome_do_arquivo"]
+        print(f"Arquivo {path_file} removido com sucesso")
+    except:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except:
+        print("Posição inválida", file=sys.stderr)
